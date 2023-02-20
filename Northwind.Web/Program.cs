040972a3f3d406665c63c864b6174de83cc5bed3
Oleksandr Shadurskyi <shadurskij@gmail.com>
2023-02-20 17:52:07 +0100

    inline delegate middleware

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -12,6 +12,26 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+    if (rep is not null)
+    {
+        Console.WriteLine($"Endpoint name: {rep.DisplayName}");
+        Console.WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+    }
+    if (context.Request.Path == "/bonjour")
+    {
+        // in the case of a match on URL path, this becomes a terminating
+        // delegate that returns so does not call the next delegate
+        await context.Response.WriteAsync("Bonjour Monde!");
+        return;
+    }
+    // we could modify the request before calling the next delegate
+    await next();
+    // we could modify the response after calling the next delegate
+});
+
 app.UseHttpsRedirection();
 
 app.UseDefaultFiles();
