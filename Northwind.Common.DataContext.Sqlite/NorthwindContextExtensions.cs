23de80f59014e278dca982abc3ded396eaa9a557
Oleksandr Shadurskyi <shadurskij@gmail.com>
2023-03-14 15:55:06 +0100

    blazor server - add Customers CRUD

--- Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
+++ Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Packt.Shared;
+
+namespace Packt.Shared;
 
 public static class NorthwindContextExtensions
 {
