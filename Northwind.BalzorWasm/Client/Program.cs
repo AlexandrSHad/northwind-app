3184d08cff0fa8e52684329af7e67db72c0ac398
Oleksandr Shadurskyi <shadurskij@gmail.com>
2023-03-14 19:09:05 +0100

    blazor wasm - added customers CRUD

--- Northwind.BalzorWasm/Client/Program.cs
+++ Northwind.BalzorWasm/Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Northwind.BalzorWasm.Client;
+using Northwind.BlazorWasm.Client.Data;
+using Packt.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<INorthwindService, NorthwindService>();
 
 await builder.Build().RunAsync();
